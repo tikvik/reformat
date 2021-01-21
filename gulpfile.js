183fbd1563c00a42325312854a56a7544d3fa836
tikvik <viktak.viktak@gmail.com>
2021-01-21 11:56:19 +0100

    favicon

--- gulpfile.js
+++ gulpfile.js
@@ -3,61 +3,8 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var pug = require('gulp-pug');
 var data = require('gulp-data');
-var fs = require('fs');
-var path = require('path');
-const debug = require('gulp-debug');
-var merge = require('merge-json');
-// var merge = require('gulp-controlled-merge-json');
 
-// Reads every image in projects folder in images
-// const folderPath = './app/images/projects/'
-// const imgPath = 'images/projects/'
-var thumbs = new Array();
-// var i = 0;
-// var j = 0;
-// var x = 0;
-// thumbs[0] = new Array();
-// thumbs[0][0] = new Array();
-// thumbs[0][1] = new Array();
-// thumbs[0][2] = "";
-// Třetí položka pole pro cestu k obrázkům
 
-// fs.readdirSync(folderPath).forEach(fileName => {
-// 	if (fileName != ".DS_Store") {
-// 		// thumbs[][]
-// 		// thumbs[i][2] = imgPath+fileName+"/"
-// 		fs.readdirSync(folderPath+fileName+"/").forEach(images => {
-// 			if (images != ".DS_Store") {
-// 				if (images != "img") {
-// 					if (images != "Icon") {
-// 						if (images != "plans") {
-// 								thumbs[i][0][j] = imgPath+fileName+"/"+images
-// 								j=j+1
-// 			}}}}
-// 		})
-
-// 		fs.readdirSync(folderPath+fileName+"/plans/").forEach(planimages => {
-// 			if (planimages != ".DS_Store") {
-// 				if (planimages != "Icon?") {
-// 					thumbs[i][1][x] = imgPath+fileName+"/plans/"+planimages
-// 					x=x+1
-// 			}}
-// 		})
-
-// 		j=0
-// 		x=0
-// 		i=i+1
-// 		thumbs[i] = new Array();
-// 		thumbs[i][0] = new Array();
-// 		thumbs[i][1] = new Array();
-// 		}
-// })
-// for (var i = 0; i < thumbs.length; i++) { 
-//     for (var j = 0; j < thumbs[i][1].length; j++)    { 
-//         console.log(thumbs[3][2]); 
-//     } 
-//     console.log("<br>"); 
-// }  
 
 function debuger() {
 	return gulp
@@ -74,20 +21,9 @@ function build() {
 					fs.readFileSync('app/data/data.json')
 					);
 			}))
-		
 
-		.pipe(pug({
-			
-			pretty: true,
-			data: {
-				"gimg" : thumbs
-			}
-
-		}))
 		.pipe(gulp.dest('dist/'));
 }
-
-
 
 
 function style() {
@@ -98,21 +34,6 @@ function style() {
 		.pipe(browserSync.stream());
 }
 
-// function slickStyle() {
-// 	return gulp
-// 		.src('app/sass/slick/*.scss') // Gets all files ending with .scss in app/scss
-// 		.pipe(sass().on('error', sass.logError))
-// 		.pipe(gulp.dest('dist/slick'))
-// 		.pipe(browserSync.stream());
-// }
-
-function assets() {
-	return gulp.src('app/**/*.{ttf,jpg,svg}').pipe(gulp.dest('./dist'));
-}
-
-
-
-
 
 function watch() {
 	browserSync.init({
@@ -121,28 +42,16 @@ function watch() {
 		}
 	});
 	gulp.watch('app/sass/**/*.sass', style);
-	// gulp.watch('app/sass/slick/**/*.scss', slickStyle);
-	gulp.watch('app/templates/**/*.pug', build);
 	gulp.watch('app/**/*.pug', build);
-	gulp.watch('app/**/*.{ttf,jpg,svg}', assets);
 	gulp.watch('dist/*.html').on('change', browserSync.reload);
 	gulp.watch('app/js/**/*.js').on('change', browserSync.reload);
 	gulp.watch('app/data/**/*.json').on('change', build);
 	gulp.watch('gulpfile.js').on('change', build);
-
-
-	// gulp.watch('app/sass/**/*.sass', push);
 }
 
-// function vue() {
-// 	return gulp.src('app/js/components/*.vue')
-// 			.pipe(vueComponent({ /* options */ }))
-// 			.pipe(gulp.dest('./dist/js/'));
-// }
 
-exports.build = build;
+// exports.build = build;
 exports.style = style;
-// exports.slickStyle = slickStyle;
 exports.assets = assets;
 exports.watch = watch;
 exports.debuger = debuger;
